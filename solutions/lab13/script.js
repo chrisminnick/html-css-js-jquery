fee4da232a785ec0947db1c5da82706edd92762b
Chris Minnick <chris@minnick.com>
2023-11-09 05:00:48 -0800

    added lab13

--- solutions/lab13/script.js
+++ solutions/lab13/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+  // Initialize the jQuery Validation plugin on the form
   $('#contactForm').validate({
+    // Specify validation rules
     rules: {
       userName: {
         required: true,
@@ -16,19 +18,28 @@ $(document).ready(function () {
         required: true,
       },
     },
+    // Specify validation error messages
     messages: {
       userName: {
-        required: 'Please enter your name.',
-        minlength: 'Your name must consist of at least 2 characters.',
+        required: 'Please enter your name',
+        minlength: 'Your name must be at least 2 characters long',
       },
       userEmail: {
-        required: 'Please enter your email address.',
-        email: 'Please enter a valid email address.',
+        required: 'Please enter your email address',
+        email: 'Please enter a valid email address',
+      },
+      userUrl: {
+        url: 'Please enter a valid URL',
       },
       userComments: {
-        required: 'Please enter your comments.',
+        required: 'Please enter your comments',
       },
     },
+    // Specify where to display the error messages
+    errorPlacement: function (error, element) {
+      error.appendTo(element.parent());
+    },
+    // Specify what happens when the form is submitted
     submitHandler: function (form) {
       form.submit();
     },
